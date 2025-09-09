2f704045309b904b6d469571422855ad4eecc9c5
shiva-017 <dasi.s@northeastern.edu>
2025-09-09 18:12:00 -0500

    prefix debug logs with dbg namespace

--- lib/debug.ts
+++ lib/debug.ts
@@ -2,6 +2,6 @@ import { DEBUG_LOGS } from './config';
 
 export function debugLog(tag: string, ...args: unknown[]) {
   if (!DEBUG_LOGS) return;
-  console.log(`[${tag}]`, ...args);
+  console.log(`[dbg:${tag}]`, ...args);
 }
 
